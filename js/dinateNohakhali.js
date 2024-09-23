340c4b3238b024ddde9221fa99b5dfcd7404c88e
mahmudul35 <101060541+mahmudul35@users.noreply.github.com>
2024-09-23 12:58:41 -0700

    added toggle button

--- js/dinateNohakhali.js
+++ js/dinateNohakhali.js
@@ -9,11 +9,28 @@ document.getElementById("Donate").addEventListener("click", () => {
   document.getElementById("totalTaka").innerText = result;
   document.getElementById("accountBalance").innerText = account;
   document.getElementById("donateNohakhali").value = "";
-  alert(result + " added");
+
   console.log(totalTaka);
 });
 
-document.getElementById("historyBtn").addEventListener("click"function()=>{
-    document.getElementById("historyBtn").classList.add("bgGray")
-    
-})
+// button toggle
+const historyBtn = document.getElementById("historyBlue");
+const donateBtn = document.getElementById("donateBtn");
+historyBtn.addEventListener("click", () => {
+  historyBtn.classList.add("bg-lime-300");
+  donateBtn.classList.remove("bg-lime-300");
+  donateBtn.classList.add("bg-gray-300");
+  document.getElementById("cardComponent").classList.add("hidden");
+});
+
+donateBtn.addEventListener("click", () => {
+  historyBtn.classList.remove("bg-lime-300");
+  donateBtn.classList.remove("bg-gray-300");
+  donateBtn.classList.add("bg-lime-300");
+
+  document.getElementById("cardComponent").classList.remove("hidden");
+});
+
+document.getElementById("blogBtn").addEventListener("click", function () {
+  window.location.href = "./bog.html";
+});
